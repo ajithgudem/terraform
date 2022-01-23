c0444203b919cee0e1470b8b92c9011fc6103a23
Ajith Gudem <agudem@oteemo.com>
2022-01-22 23:28:53 -0500

    Moving security groups to seperate template

--- chapter2/elb.tf
+++ chapter2/elb.tf
@@ -20,24 +20,3 @@ resource "aws_elb" "instance_elb" {
     timeout             = 3
   }
 }
-
-resource "aws_security_group" "elb_sg" {
-  name = "terraform-elb-ingress"
-  # Allow all outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  # Inbound HTTP from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-        Name = "allow-http"
-    }
-}
--- chapter2/security.tf
+++ chapter2/security.tf
@@ -10,3 +10,24 @@ resource "aws_security_group" "instance-sg" {
         Name = "allow-http"
     }
 }
+
+resource "aws_security_group" "elb_sg" {
+  name = "terraform-elb-ingress"
+  # Allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+        Name = "allow-http"
+    }
+}
